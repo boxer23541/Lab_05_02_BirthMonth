d6f5dd387c418c27389524a61fbd4390672cebaf
boxer23541 <alexmb2354@gmail.com>
2022-02-09 10:52:15 -0500

    Finished

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,15 @@ public class Main
 
     public static void main(String[] args)
     {
-	int birthMonth = 5;
+	int birthMonth = 13;
 
-    
+    if(birthMonth >= 1 && birthMonth <= 12)
+    {
+        System.out.println("Your birth month is " + birthMonth);
+    }
+    else
+    {
+        System.out.println("Invalid Input, please enter a number 1-12");
+    }
     }
 }
